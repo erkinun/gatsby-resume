611226e1e9b4fcc988264058852b5e7a70344486
Erkin <erkinun@gmail.com>
2020-04-22 22:10:38 +0100

    customize the welcome page

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import resume from '../../data/profile.json'
 
 const Paragraph = styled.p`
   width: 600px;
@@ -10,10 +11,15 @@ const Paragraph = styled.p`
 const Section = styled.section`
   width: 1000px;
 `
+const Welcome = styled.p`
+  font-size: 2rem;
+`
 
+// TODO rename this to main content? home landing?
 const Summary = ({ data }) => (
   <Section className="py-5 border-b border-neutral-300 lg:flex items-center">
     <Paragraph className="text-center tracking-wide leading-relaxed lg:text-left lg:text-lg">
+      <Welcome>Hi, I'm {resume.fullname}.</Welcome>
       {data}
     </Paragraph>
   </Section>
