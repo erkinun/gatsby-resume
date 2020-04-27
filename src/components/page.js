e6dad4fbf82377e4780a0892bab75a806f753ca6
Erkin <erkinun@gmail.com>
2020-04-27 21:44:23 +0100

    removed contacts from headers, removed phone number

--- src/components/page.js
+++ src/components/page.js
@@ -3,7 +3,6 @@ import { Header, Footer, Navigation, SEO } from '../components'
 import resume from '../../data/profile'
 import '../main.css'
 
-// TODO remove contact details from header?
 // TODO move avatar to main screen
 // TODO pass down title as a props to seo
 // TODO check out how tailwind works
@@ -17,11 +16,7 @@ const Page = ({ render }) => (
   <main className="antialiased text-neutral-900 bg-neutral-100 min-h-screen sm:p-5">
     <SEO title="Resume" />
     <div className="container mx-auto shadow bg-white py-5 px-10">
-      <Header
-        contacts={resume.contact}
-        name={resume.fullname}
-        role={resume.role}
-      />
+      <Header name={resume.fullname} role={resume.role} />
       <div className="page-content main">
         <Navigation />
         <section className="py-5 border-b border-neutral-300 lg:flex items-center">
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,7 @@ const Home = () => (
   <main className="antialiased text-neutral-900 bg-neutral-100 min-h-screen sm:p-5">
     <SEO title="Home" />
     <div className="container mx-auto shadow bg-white py-5 px-10">
-      <Header
-        contacts={resume.contact}
-        name={resume.fullname}
-        role={resume.role}
-      />
+      <Header name={resume.fullname} role={resume.role} />
       <div className="main">
         <Navigation />
         <Landing data={resume.summary} />
