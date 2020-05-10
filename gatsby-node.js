5cf515782cf6c1a743570e8a3f6c607c201efe61
Erkin <erkinun@gmail.com>
2020-05-10 22:02:10 +0100

    made markdown render as a page with some issues

---
+++ gatsby-node.js
@@ -0,0 +1,41 @@
+const path = require(`path`)
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions
+
+  const blogPostTemplate = path.resolve(`src/components/blog-template.jsx`)
+
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: blogPostTemplate,
+      context: {
+        slug: node.frontmatter.slug,
+        pathForBlog: node.frontmatter.path,
+      }, // additional data can be passed via context
+    })
+  })
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,9 +3,13 @@ import React from 'react'
 import { Page } from '../components'
 
 // TODO find a good starter but use the same style from resume
+// TODO looks like markdown is not that supported?
 // TODO find a way to export blogs from medium, or manually export them to netfly and use
-const Blog = () => (
-  <Page title="Blog" render={() => <div>Blogs will be shown here</div>} />
-)
+const Blog = ({ data }) => {
+  console.log('grapghl q here?: ', data)
+  return (
+    <Page title="Blog" render={() => <div>Blogs will be shown here</div>} />
+  )
+}
 
 export default Blog
