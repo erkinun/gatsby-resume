8f42dddac60cba5d659c92a48e3ed8dc9216c26b
Erkin <erkinun@gmail.com>
2020-06-27 16:52:55 +0100

    storybook setup

--- src/pages/books.js
+++ src/pages/books.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 import { Page } from '../components'
 
-// TODO use the cms tool somehow to export data from either kindle or goodreads
+// TODO use the cms tool somehow to
+// TODO export data from either kindle or goodreads
+// TODO will require sorting
+// TODO implement search
 const Books = () => (
   <Page title="Books" render={() => <div>Books will be done here</div>} />
 )
